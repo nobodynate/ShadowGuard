998d444e503f386848ef70c39d0ea36a2db82cba
nate <n.wilson.22.6.8@gmail.com>
2023-05-07 01:03:58 -0500

    need linux to test in

--- shadowguard.py
+++ shadowguard.py
@@ -1,15 +1,34 @@
-class SHADOWGUARD:
+import subprocess
+
+class SHADOWGUARD():
     def __init__(self):
-        latest_db = get_kismet_db()
-        zero_to_five = None
-        five_to_ten = None
+        self.interfaces = None
+        self.latest_db = self.get_kismet_db()
+        self.zero_to_five = None
+        self.five_to_ten = None
         pass
 
     def init_interfaces(self):
         pass
 
-    def check_kismet(self):
-        pass
+    def monitor(self):
+        """Checks that kismet is running and monitor mode enabled"""
+        # Is Kismet Running?
+        processes = subprocess.check_output('ps aux | grep kismet').decode('utf-8').splitlines()
+        if len(processes) <= 2:
+            raise "Kismet is not running!"
+            exit()
+        
+        # Monitor Mode enabled?
+        monitoring_interfaces = []
+        for interface in self.interfaces:
+            iw_output = subprocess.check_output('iwconfig').decode('utf-8')
+            if "Mode:Monitor" not in iw_output:
+                raise f"Monitor mode not enabled on {interface}"
+                exit()
+
+        # Return true if both checks pass
+        return True
 
     def start_kismet(self):
         pass
---
+++ test_shadowguard.py
@@ -0,0 +1,5 @@
+from shadowguard import SHADOWGUARD as sg
+
+def test_function():
+    sg.monitor()
+    assert True
