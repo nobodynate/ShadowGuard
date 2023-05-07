0b146d272576cb55609bd4bc0417d1c88685f4ce
Nate <77979206+nobodynate@users.noreply.github.com>
2023-05-07 00:24:40 -0500

    ready to start tdd

---
+++ shadowguard.py
@@ -0,0 +1,31 @@
+class SHADOWGUARD:
+    def __init__(self):
+        latest_db = get_kismet_db()
+        zero_to_five = None
+        five_to_ten = None
+        pass
+
+    def init_interfaces(self):
+        pass
+
+    def check_kismet(self):
+        pass
+
+    def start_kismet(self):
+        pass
+
+    def get_kismet_db(self):
+        pass
+
+    def query_kismet(self):
+        pass
+
+    def get_macs_from(self, start_time, end_time):
+        pass
+    
+    def get_ssid_from(self, start_time, end_time):
+        pass
+
+    def rotate_lists(self):
+        pass
+
